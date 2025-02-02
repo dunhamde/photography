e99fbc72acfca67ea05c2d79c4328407b6a3c018
Derek Dunham <dunham.derek@gmail.com>
2025-02-01 22:23:27 -0700

    scuffed infinite scrolling with dom manip

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const prev = document.getElementById("prev");
-console.log(prev);
 const next = document.getElementById("next");
 const list = document.getElementById("items");
 const itemWidth = 500;
+let count = 0;
 
 prev.addEventListener("click", () => {
   list.scrollLeft -= itemWidth;
 });
 next.addEventListener("click", () => {
   list.scrollLeft += itemWidth;
+
+  count++;
+  if (count === 5) {
+    count = 1;
+  }
+  const removeFirstItem = document.getElementById(`box${count}`);
+  const clone = removeFirstItem.cloneNode(true);
+  removeFirstItem.remove();
+  list.appendChild(clone);
 });
+
+// when the user clicks, remove and image from the front, add a new one to the back
+
+// setInterval(() => {
+//   list.scrollLeft += itemWidth;
+//   count++;
+//   if (count === 5) {
+//     count = 1;
+//   }
+//   const removeFirstItem = document.getElementById(`box${count}`);
+//   const clone = removeFirstItem.cloneNode(true);
+//   removeFirstItem.remove();
+//   list.appendChild(clone);
+// }, 1000);
