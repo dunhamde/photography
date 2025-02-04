834e6f83c74372b5782683fa275753d988bb3873
Derek Dunham <dunham.derek@gmail.com>
2025-02-04 16:49:44 -0700

    pause animation on hover

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const list = document.getElementById("items");
-const itemWidth = 500;
-let count = 0;
+const gallery = document.getElementById("gallery");
+const slides = document.querySelectorAll(".gallery-slide");
 
-prev.addEventListener("click", () => {
-  list.scrollLeft -= itemWidth;
-});
-next.addEventListener("click", () => {
-  list.scrollLeft += itemWidth;
-
-  count++;
-  if (count === 5) {
-    count = 1;
-  }
-  const removeFirstItem = document.getElementById(`box${count}`);
-  const clone = removeFirstItem.cloneNode(true);
-  removeFirstItem.remove();
-  list.appendChild(clone);
+gallery.addEventListener("mouseover", () => {
+  slides.forEach((element) => {
+    element.style.animationPlayState = "paused";
+  });
 });
 
-// when the user clicks, remove and image from the front, add a new one to the back
-
-// setInterval(() => {
-//   list.scrollLeft += itemWidth;
-//   count++;
-//   if (count === 5) {
-//     count = 1;
-//   }
-//   const removeFirstItem = document.getElementById(`box${count}`);
-//   const clone = removeFirstItem.cloneNode(true);
-//   removeFirstItem.remove();
-//   list.appendChild(clone);
-// }, 1000);
+gallery.addEventListener("mouseout", () => {
+  slides.forEach((element) => {
+    element.style.animationPlayState = "running";
+  });
+});
