ed01162c2a57985f447112e2973436aa180a9c8a
Derek Dunham <dunham.derek@gmail.com>
2025-02-17 18:05:09 -0700

    add deploy scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+#rm -rf public
+mkdir public
+cp -r css public/css
+cp -r img public/img
+cp -r pages public/pages
+cp index.html public/index.html
+cp 404.html public/404.html
+cp script.js public/script.js
