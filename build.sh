de90cbdebb2b24f689d5f6edffbfc11b001a1ef6
Derek Dunham <dunham.derek@gmail.com>
2025-02-17 18:47:36 -0700

    fix double col styuling

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#rm -rf public
+rm -rf public
 mkdir public
 cp -r css public/css
 cp -r img public/img
@@ -6,3 +6,4 @@ cp -r pages public/pages
 cp index.html public/index.html
 cp 404.html public/404.html
 cp script.js public/script.js
+cp config.js public/config.js
